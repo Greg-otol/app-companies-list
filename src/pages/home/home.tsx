28dd05e72c5045fc7c7445710dbc143d0dd29f86
Gregório Neto <gregoriogrgeotavia@hotmail.com>
2023-07-14 14:56:41 -0300

    chore: home component implementation

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,77 @@
+import { useEffect, useState } from "react";
+import { Header } from "../../components/header/header";
+import { getAllCompanies } from "../../services/companyServices/getAllCompanies";
+import { ICompany } from "../../interfaces/ICompany";
+import { TableCompanies } from "../../components/tableCompanies/tableCompanies";
+import { HomeContainer } from "./styleHome";
+
 export function Home() {
+  const [filterStatus, setFilterStatus] = useState(0);
+  const [companies, setCompanies] = useState<ICompany[]>([]);
+  const [dataInicioFiltro, setDataInicioFiltro] = useState("");
+  const [dataFinalFiltro, setDataFinalFiltro] = useState("");
+
+  useEffect(() => {
+    loadCompanies();
+  }, []);
+
+  const loadCompanies = async () => {
+    try {
+      const response = await getAllCompanies();
+      setCompanies(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  let companiesFiltered = companies;
+
+  if (filterStatus === 1) {
+    companiesFiltered = companies.filter(
+      (company) => company.statusEmpresa === 1
+    );
+  } else if (filterStatus === 2) {
+    companiesFiltered = companies.filter(
+      (company) => company.statusEmpresa === 2
+    );
+  }
+
+  if (dataInicioFiltro && dataFinalFiltro) {
+    companiesFiltered = companiesFiltered.filter((company) => {
+      const dataInclusaoEmpresa = new Date(company.dataInclusao);
+      const dataInicioFiltroDate = new Date(dataInicioFiltro);
+      const dataFinalFiltroDate = new Date(dataFinalFiltro);
+
+      return (
+        dataInclusaoEmpresa >= dataInicioFiltroDate &&
+        dataInclusaoEmpresa <= dataFinalFiltroDate
+      );
+    });
+  }
+
+  const handleFilterChange = (status: number) => {
+    setFilterStatus(status);
+  };
+
+  const handleDataInicioFiltroChange = (dataInicio: string) => {
+    setDataInicioFiltro(dataInicio);
+  };
+
+  const handleDataFinalFiltroChange = (dataFinal: string) => {
+    setDataFinalFiltro(dataFinal);
+  };
+
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <HomeContainer>
+      <Header
+        onFilterChange={handleFilterChange}
+        companiesFiltered={companiesFiltered}
+        onDataInicioFiltroChange={handleDataInicioFiltroChange}
+        onDataFinalFiltroChange={handleDataFinalFiltroChange}
+        dataInicioFiltro={dataInicioFiltro}
+        dataFinalFiltro={dataFinalFiltro}
+      />
+      <TableCompanies data={companiesFiltered} />
+    </HomeContainer>
   );
 }
---
+++ src/pages/home/styleHome.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.div`
+  display: flex;
+  width: 100%;
+  padding: 70px 10px 10px 10px;
+
+  @media (max-width: 700px) {
+    padding: 115px 10px 10px 10px;
+  }
+`;
