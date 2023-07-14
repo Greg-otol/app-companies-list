b2a4cf9065bd605a7e5f259f59e73cc3d89b99d6
Gregório Neto <gregoriogrgeotavia@hotmail.com>
2023-07-14 13:53:57 -0300

    feat: create interface company

---
+++ src/interfaces/ICompany.ts
@@ -0,0 +1,12 @@
+export interface ICompanies {
+  id: string;
+  codigo: string;
+  razaoSocial: string;
+  cnpj: string;
+  inscricaoMunicipal: string;
+  responsavelLegal: string;
+  email: string;
+  telefoneContato: string;
+  statusEmpresa: number;
+  dataInclusao: string;
+}
